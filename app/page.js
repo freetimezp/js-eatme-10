73573f081554b2e8604cad2e0e379fc48a0b0f70
freetimezp <kraftukrnet@ukr.net>
2023-12-17 23:54:33 +0200

    04. js-eatme-10. Header Component. Show, hide menu on mobile.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import StyleGuide from "@/components/StyleGuide";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
       {/* <StyleGuide /> */}
+      <Header />
       <Hero />
       <div className="h-[4000px]"></div>
     </main>
---
+++ components/Nav.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const links = [
+    {
+        path: 'home',
+        name: 'home',
+        offset: -50,
+    },
+    {
+        path: 'menu',
+        name: 'menu',
+        offset: -50,
+    },
+    {
+        path: 'about',
+        name: 'about',
+        offset: -150,
+    },
+    {
+        path: 'contact',
+        name: 'contact',
+        offset: 0,
+    },
+];
+
+const Nav = ({ containerStyles, linkStyles }) => {
+    return (
+        <nav className={`${containerStyles}`}>
+            {links.map((link, index) => (
+                <Link
+                    to={link.path}
+                    key={index}
+                    spy={true}
+                    smooth={true}
+                    offset={link.offset}
+                    duration={500}
+                    className={`${linkStyles}`}
+                >
+                    {link.name}
+                </Link>
+            ))}
+        </nav>
+    );
+};
+
+export default Nav;
